41d41d11c80349f502041b52da035fc1b314d191
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-26 07:59:29 -0700

    Initialisation

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * printf - produces output according to a format
+ * @format - a character string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
