ef660b764ee47d0b279235652b3e44f9cd9f41ef
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-28 12:58:21 -0700

    Created print_int.c for apecifiers d and i

---
+++ print_int.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_int - prints an integer
+ * @list : list of arguements
+ *
+ * Return: number of printed characters
+ */
+int print_int(va_list list)
+{
+	unsigned int print_count, n, div, num;
+
+	n = va_arg(list, int);
+	div = 1;
+	len = 0;
+
+	if (n <  0)
+	{
+		_putchar('-');
+		num = n * -1;
+
+		print_count = print_count + 1;
+	}
+	else
+		num = n;
+
+	while ((num / div) > 9)
+		div = div * 10;
+
+	while (div != 0)
+	{
+		_putchar('0' + (num / div));
+		num = num % div;
+		div = div / 10;
+
+		print_count = print_count + 1;
+	}
+
+	return (print_count);
+}
