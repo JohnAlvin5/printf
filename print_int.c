17bcd5b6469ca9da71e20cd371bfd4d59b0e0361
Josephinemwende <josymwesh45@gmail.com>
2023-03-28 14:27:42 -0700

    handle conversion specifiers

--- print_int.c
+++ print_int.c
@@ -8,22 +8,22 @@
  */
 int print_int(va_list list)
 {
-	int print_count, n, div;
+	int print_count, x, div;
 	unsigned int num;
 
-	n = va_arg(list, int);
+	x = va_arg(list, int);
 	div = 1;
 	print_count = 0;
 
-	if (n < 0)
+	if (x < 0)
 	{
 		_putchar('-');
-		num = n * -1;
+		num = x * -1;
 
 		print_count = print_count + 1;
 	}
 	else
-		num = n;
+		num = x;
 
 	while ((num / div) > 9)
 		div = div * 10;
